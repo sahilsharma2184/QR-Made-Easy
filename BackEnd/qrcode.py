85ad6445bbb22e48eb63a7f2dc698747ae8e525c
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-10-27 19:44:13 +0530

    updated backend code

--- BackEnd/qrcode.py
+++ BackEnd/qrcode.py
@@ -1,5 +1,5 @@
 import requests #importing requests library to validate the URL which will be entered by the user
-
+import time #importing time library to make the code wait wherever required
 print("Welcome to QR Made Easy! Glad to see you here!\n") #Welcome display message
 
 print("What should I call you?") #asking for user's name
@@ -7,12 +7,22 @@ name=input() #taking input for name
 
 print("\nHello "+name+", nice to meet you!\n") #Greeting user
 
-print(name+", what would you like to generate a QR code for today?") #Asking url for which QR is to be generated
-url=input() #taking input for the url
+while True:
+    print(name+", what would you like to generate a QR code for today?") #Asking url for which QR is to be generated
+    url=input() #taking input for the url
 
-try:
-    response=requests.get(url) #requests.get(url) sends a GET request to the entered url and tries to access the url entered, further the response variable stores the status code that is received back.
-    if response.status_code == 200:
-        
-
+    if not url.startswith("https://") or not url.startswith("http://"): #checking if the url is 
+        print(name+" I guess that you have entered wrong url, kindly check that and enter again!")
+    try:
+        response=requests.get(url) #requests.get(url) sends a GET request to the entered url and tries to access the url entered, further the response variable stores the status code that is received back.
+        if response.status_code == 200: #if condition to check that whether the site is responsding not 
+            print(name+", the url you entered is valid!\n")
+            print("The processing of QR image and text file has started..")
+            time.sleep(2)
+            break
+        else:
+            print(name+" I guess that you have entered wrong url, kindly check that and enter again!")
+            url=input()
+    except requests.exceptions.RequestException as error:
+        print("Error occured: ",error)
 
